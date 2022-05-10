6a29870a53e64daeee10c5fed66a2586957481c3
pablodamascenoo <pablofacex@gmail.com>
2022-05-09 22:02:57 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Reset from "../assets/global_styles/Reset";
+
+export default function App() {
+  return (
+    <>
+      <Reset />
+      <h1>Hello World</h1>
+    </>
+  );
+}
