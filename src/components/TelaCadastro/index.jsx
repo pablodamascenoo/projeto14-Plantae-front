7fbb13bdd5efffef89d1839747e8b0bc1247aa45
pablodamascenoo <pablofacex@gmail.com>
2022-05-11 20:01:38 -0300

    feat: tela de cadastro concluída

--- src/components/TelaCadastro/index.jsx
+++ src/components/TelaCadastro/index.jsx
@@ -1,21 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 import { Container } from "./style";
 
 export default function TelaCadastro() {
+  const [cadastro, SetCadastro] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+    senha2: "",
+  });
+  const [enviado, SetEnviado] = useState(false);
+  const navigate = useNavigate();
+
+  const { nome, email, senha, senha2 } = cadastro;
+
+  function enviarCadastro(e) {
+    e.preventDefault();
+    SetEnviado(true);
+
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!emailRegex.test(cadastro.email)) {
+      alert("O email deve ser preenchido corretamente");
+      SetEnviado(false);
+      return;
+    }
+
+    if (cadastro.senha !== cadastro.senha2) {
+      alert("As senhas devem coincidir");
+      SetEnviado(false);
+      return;
+    }
+
+    const promisse = axios.post("https://plantae.herokuapp.com/cadastro", {
+      nome,
+      email,
+      senha,
+      senha2,
+    });
+
+    promisse.then(() => {
+      SetEnviado(false);
+      navigate("/auth/login");
+    });
+
+    promisse.catch((erro) => {
+      alert(erro.response.data);
+      console.log(e);
+      SetEnviado(false);
+    });
+  }
+
   return (
-    <Container>
+    <Container
+      preenchido={!(nome && email && senha && senha2)}
+      enviado={enviado}
+    >
       <h1>Plantae</h1>
 
-      <div>
-        <h2>Criar uma conta</h2>
-        <form action="">
-          <input type="text" placeholder="nome" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="senha" />
-          <input type="password" placeholder="confirme sua senha" />
-        </form>
-      </div>
+      <form onSubmit={enviarCadastro}>
+        <input
+          disabled={enviado}
+          required
+          type="text"
+          placeholder="nome"
+          value={nome}
+          onChange={(e) => {
+            SetCadastro({ ...cadastro, nome: e.target.value });
+          }}
+        />
+        <input
+          disabled={enviado}
+          required
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => {
+            SetCadastro({ ...cadastro, email: e.target.value });
+          }}
+        />
+        <input
+          disabled={enviado}
+          required
+          type="password"
+          placeholder="senha"
+          value={senha}
+          onChange={(e) => {
+            SetCadastro({ ...cadastro, senha: e.target.value });
+          }}
+        />
+        <input
+          disabled={enviado}
+          required
+          type="password"
+          placeholder="confirme sua senha"
+          value={senha2}
+          onChange={(e) => {
+            SetCadastro({ ...cadastro, senha2: e.target.value });
+          }}
+        />
+        <button type="submit" disabled={!(nome && email && senha && senha2)}>
+          Cadastrar
+        </button>
+      </form>
     </Container>
   );
 }
--- src/components/TelaCadastro/style.js
+++ src/components/TelaCadastro/style.js
@@ -14,4 +14,38 @@ export const Container = styled.main`
     font-weight: 700;
     color: #1da154;
   }
+
+  form {
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    gap: 10px;
+
+    input,
+    button {
+      height: 60px;
+      border-radius: 25px;
+      max-width: 365px;
+      width: 90%;
+      font-size: 20px;
+      font-family: "Lato", sans-serif;
+    }
+
+    input {
+      border: 3px solid #20b25d;
+      padding-left: 15px;
+      background-color: ${(props) =>
+        props.enviado ? "rgba(0, 0, 0, 0.2)" : "white"};
+    }
+
+    button {
+      border: none;
+      background-color: ${(props) => (props.preenchido ? "grey" : "#20b25d")};
+      cursor: ${(props) => (props.preenchido ? "initial" : "pointer")};
+      font-weight: 700;
+      color: white;
+    }
+  }
 `;
