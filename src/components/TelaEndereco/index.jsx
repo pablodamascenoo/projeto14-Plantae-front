043bb826c1675d380f51124461cad714d3830781
pablodamascenoo <pablofacex@gmail.com>
2022-05-16 10:13:11 -0300

    feat: terminada a a tela de endereco

--- src/components/TelaEndereco/index.jsx
+++ src/components/TelaEndereco/index.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Header from "../Header";
 import { Box, Input, Button } from "./style";
 import AddressContext from "../../contexts/AddressContext";
+import UserContext from "../../contexts/UserContext";
 
 export default function TelaEndereco() {
   const [endereco, SetEndereco] = useState({
@@ -15,7 +17,42 @@ export default function TelaEndereco() {
     estado: "",
     complemento: "",
   });
-  const { address, SetAddress } = useContext(AddressContext);
+  const { SetAddress } = useContext(AddressContext);
+  const { userInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userInfo?.token}`,
+    },
+  };
+
+  useEffect(() => {
+    if (!userInfo) {
+      alert("o usuário deve estar logado para entrar nessa página!");
+      navigate("/auth/login");
+      return;
+    }
+
+    const promisse = axios.get(
+      "https://plantae.herokuapp.com/carrinho",
+      config
+    );
+
+    promisse.then((obj) => {
+      const { data } = obj;
+      if (!data.length) {
+        alert("O usuário não possui itens no carrinho");
+        navigate("/");
+        return;
+      }
+    });
+
+    promisse.catch((erro) => {
+      alert(erro.response.data);
+      navigate("/");
+      return;
+    });
+  }, []);
 
   function enderecoVazio() {
     const keys = Object.keys(endereco);
@@ -57,8 +94,14 @@ export default function TelaEndereco() {
   function armazenaEndereco(e) {
     e.preventDefault();
 
+    if (enderecoVazio()) {
+      alert("preencha os campos corretamente");
+      return;
+    }
+
     localStorage.setItem("Address", JSON.stringify({ ...endereco }));
     SetAddress({ ...endereco });
+    navigate("/checkout/pagamento");
   }
 
   return (
