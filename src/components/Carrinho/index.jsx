f8ae52d899c3afe668df72cd66ce2b08b81a00b8
Analice <anadias.alice@gmail.com>
2022-05-16 00:58:52 -0300

    Feat: Criando componente do carrinho

--- src/components/Carrinho/index.jsx
+++ src/components/Carrinho/index.jsx
@@ -1,14 +1,66 @@
 import React from "react";
 import Header from "../Header";
 import { Container } from "./style";
-
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext";
+import carrinho from "../../assets/SVGs/cart-outline.svg"
+import lixo from "../../assets/SVGs/trash-bin-outline.svg"
+import { Link } from "react-router-dom";
 
 export default function Carrinho() {
-    return(
-        <Container>
-        <Header />
-        <h1>carrinho</h1>
-        </Container>
-        
-    );
+    
+    const { userInfo } = useContext(UserContext);
+    const token = userInfo?.token;
+    const nomeUsuario = userInfo?.nome;
+
+    
+
+    if(token){
+        console.log(`token = ${token}`)
+        return(
+            <>
+            <Header />
+            <Container>
+                <h1>Seu carrinho, {nomeUsuario}: </h1>
+                <div className="listaDeItens">
+                    <div className="item">
+                        <img />
+                        <div className="infos">
+                            <span>(qntx) nome</span>
+                            <span>R$</span>
+                        </div>
+                        <img src={lixo} alt="Excluir" className="excluir"/>
+                    </div>
+
+                    <div className="item">
+                        <img />
+                        <div className="infos">
+                            <span>(qntx) nome</span>
+                            <span>R$</span>
+                        </div>
+                        <img src={lixo} alt="Excluir" className="excluir"/>
+                    </div>
+                </div>
+            </Container>
+            </>
+            
+            
+        );
+    }else {
+        console.log("sem token;")
+        return(
+            <Container>
+                <div className="deslogado">
+                    <img src={carrinho} alt="Carrinho" className="carrinho"/>
+                    <Link to={"/auth/login"}>
+                        <button>Faça login para acessar o carrinho</button>
+                    </Link>
+                </div>
+                
+            </Container>
+            
+        );
+    }
+    
+    
 }
--- src/components/Carrinho/style.jsx
+++ src/components/Carrinho/style.jsx
@@ -1,4 +1,72 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+position: relative;
+
+min-height: 100%;
+margin-top: 30px;
+background: pink;
+
+font-family: 'Lato', sans-serif;
+
+    h1{
+        margin-bottom: 15px;
+
+        font-weight: 700;
+        font-size: 20px;
+
+        width: 90%;
+    }
+
+    img {
+        width: 70px;
+    }
+
+    .listaDeItens {
+        width: 90%;
+        background: blue;
+    }
+
+    .item {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        width: 100%;
+
+        padding-top: 10px;
+        padding-bottom: 10px;
+        margin-bottom: 15px;
+        background: green;
+    }
+
+    .infos {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .excluir {
+        width: 30px;
+    }
+
+    .carrinho {
+        width: 170px;
+    }
+
+    .deslogado {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50% , -50%);
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+    }
 `
